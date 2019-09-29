52359f61fc5627f481bbb9a8afc2a40ff1749f5b
Sarah Geara <sarah.geara@isae.edu.lb>
2019-09-29 23:43:14 +0300

    Implement appointment types activity actions and setup views.

---
+++ app/src/main/java/com/example/projetc2application/activities/AppointmentTypesActivity.java
@@ -0,0 +1,54 @@
+package com.example.projetc2application.activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RelativeLayout;
+
+import com.example.projetc2application.R;
+import com.example.projetc2application.adapters.AppointmentTypesAdapter;
+import com.example.projetc2application.adapters.DoctorsAdapter;
+import com.example.projetc2application.asyncs.GetAppointmentTypesAsync;
+import com.example.projetc2application.beans.AppointmentBean;
+import com.example.projetc2application.beans.DoctorsBean;
+
+import java.util.ArrayList;
+
+public class AppointmentTypesActivity extends AppCompatActivity {
+
+    Activity activity;
+    RelativeLayout rlProgressBar;
+    RecyclerView recyclerView;
+    AppointmentTypesAdapter appointmentTypesAdapter;
+    GetAppointmentTypesAsync getAppointmentTypesAsync;
+    GetAppointmentTypesAsync.OnFinishListener onFinishListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_types);
+
+        activity = this;
+        rlProgressBar = findViewById(R.id.rlProgressBar);
+        recyclerView = findViewById(R.id.rvAppointmentTypes);
+        onFinishListener = new GetAppointmentTypesAsync.OnFinishListener() {
+            @Override
+            public void onSuccess(Object var1) {
+                ArrayList<AppointmentBean> newsBeans = (ArrayList<AppointmentBean>)var1;
+                appointmentTypesAdapter = new AppointmentTypesAdapter(activity,newsBeans);
+                recyclerView.setAdapter(appointmentTypesAdapter);
+            }
+
+            @Override
+            public void onError(Object var1) {
+
+            }
+        };
+
+        getAppointmentTypesAsync = new GetAppointmentTypesAsync(activity, rlProgressBar, false, onFinishListener);
+        getAppointmentTypesAsync.execute();
+
+
+    }
+}
