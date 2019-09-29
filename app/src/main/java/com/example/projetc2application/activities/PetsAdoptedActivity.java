3040ee1dbae36d74b8a51d21b1b4c7d94bd1d931
Sarah Geara <sarah.geara@isae.edu.lb>
2019-09-29 23:52:00 +0300

    Implement pets adopted activity and setup views and listeners .

---
+++ app/src/main/java/com/example/projetc2application/activities/PetsAdoptedActivity.java
@@ -0,0 +1,63 @@
+package com.example.projetc2application.activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RelativeLayout;
+
+import com.example.projetc2application.R;
+import com.example.projetc2application.adapters.PetsAdapter;
+import com.example.projetc2application.asyncs.GetPetsToAdoptAsync;
+import com.example.projetc2application.asyncs.GetPetsUserAsync;
+import com.example.projetc2application.beans.PetsBean;
+
+import java.util.ArrayList;
+
+public class PetsAdoptedActivity extends AppCompatActivity {
+
+    RecyclerView rvNews;
+    RelativeLayout rlProgressBar;
+    Activity activity;
+    PetsAdapter newsAdapter;
+    GetPetsToAdoptAsync getNewsAsync;
+    //    GetPetsUserAsync getNewsAsync;
+//    GetPetsUserAsync.OnFinishListener onFinishListener;
+    GetPetsToAdoptAsync.OnFinishListener onFinishListener;
+    int page = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets_adopted);
+
+        setupView();
+    }
+
+    public void setupView() {
+        activity = this;
+        rvNews = findViewById(R.id.rvNews);
+        rlProgressBar = findViewById(R.id.rlProgressBar);
+        onFinishListener = new GetPetsToAdoptAsync.OnFinishListener() {
+            @Override
+            public void onSuccess(Object var1) {
+                ArrayList<PetsBean> newsBeans = (ArrayList<PetsBean>) var1;
+                newsAdapter = new PetsAdapter(activity, newsBeans);
+                rvNews.setAdapter(newsAdapter);
+            }
+
+            @Override
+            public void onError(Object var1) {
+
+            }
+        };
+
+        getNews(page, false);
+    }
+
+    public void getNews(int page, boolean isRefresh) {
+        getNewsAsync = new GetPetsToAdoptAsync(activity, rlProgressBar, false, onFinishListener);
+        getNewsAsync.execute();
+    }
+
+}
